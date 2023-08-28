06c078fd8e7b5bf8740fa7ec50cfdacb900595c8
mankoan022 <mankoana022@student.wethinkcode.co.za>
2023-08-28 15:13:06 +0200

    added scanner object and inputs

--- src/main/java/Projects/Calc.java
+++ src/main/java/Projects/Calc.java
@@ -1,7 +1,15 @@
 package Projects;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+
+        System.out.println("Please enter on of this operators: [ *, +, /, - ]");
     }
 }
