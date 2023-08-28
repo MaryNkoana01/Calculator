ff9d68f26736e11ae2ca3ab6b4b7ca167f6a7102
mankoan022 <mankoana022@student.wethinkcode.co.za>
2023-08-28 17:34:44 +0200

    added available operators

--- src/main/java/Projects/Calc.java
+++ src/main/java/Projects/Calc.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
+        System.out.println("================================================================");
 
-        System.out.println("             SIMPLE CALCULATOR....");
+        System.out.println("            [....SIMPLE CALCULATOR....]    ");
 
         System.out.println("================================================================");
 
         Scanner scan = new Scanner(System.in);
 
-        int number1 = scan.nextInt();
-        int number2 = scan.nextInt();
+        System.out.println("Please use one of this operators: [ *, +, /, - ]");
 
-        System.out.println("Please enter on of this operators: [ *, +, /, - ]");
+        System.out.println("Enter the first number: ");
+        int First_number = scan.nextInt();
+        System.out.println("Enter the second number: ");
+        int Second_number = scan.nextInt();
+
     }
 }
